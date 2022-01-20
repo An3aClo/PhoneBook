0bc2b6742e0d805ff7485b8bc40f93877a3ab0d6
Andrea Cloete <andrea.cloete@fullstack.co.za>
2022-01-20 18:19:49 +0200

    Added Get Phone Books API

--- Controllers/PhoneBook.cs
+++ Controllers/PhoneBook.cs
@@ -1,16 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PhoneBook.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Data;
+using System.Collections;
 
 namespace PhoneBook.Controllers
 {
-    public class PhoneBook : Controller
+    [ApiController]
+    [Route("Api/[controller]")]
+    public class PhoneBook : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet, Route("GetPhoneBooks")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public List<PhoneBookObject> Post()
+        {            
+            return GetPhoneBooks();
+        }
+
+        public List<PhoneBookObject> GetPhoneBooks()
         {
-            return View();
+            List<PhoneBookObject> allPhoneBooks = new List<PhoneBookObject>();
+            try
+            {
+                DataRowCollection rows = DAL.ExecuteSP("GetAllPhoneBooks").Tables[0].Rows;
+                if (rows.Count > 0)
+                {
+                    foreach (DataRow row in rows)
+                    {
+                        var phoneBook = new PhoneBookObject()
+                        {
+                            PhoneBookID = Guid.Parse(row["PhoneBookID"].ToString()),
+                            PhoneBookName = row["PhoneBookName"].ToString(),
+                        };
+                        allPhoneBooks.Add(phoneBook);
+                    }
+                }
+                return allPhoneBooks;
+            }
+            catch (Exception ex)
+            {
+                throw;
+                return null;
+            }
         }
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 using PhoneBook.Models;
 using System.Data;
 using System.Collections;
+using Newtonsoft.Json;
+using System.Net.Http;
 
 namespace PhoneBook.Pages
 {
@@ -27,9 +29,9 @@ namespace PhoneBook.Pages
 
         public void OnGet()
         {
-            //TODO :: Call API here  
-            //Fetch all phone books
-            GetPhoneBooks();
+            //Fetch all phone books 
+            AllPhoneBooks = GetPhoneBooksAPICall().Result;
+            
             if (AllPhoneBooks.Count>0)
             {
                 SelectedPhoneBook = AllPhoneBooks.First();
@@ -40,35 +42,10 @@ namespace PhoneBook.Pages
 
         public void OnPostFetchPhoneBookContact(Guid phoneBookId)
         {
-            GetPhoneBooks();
+            AllPhoneBooks = GetPhoneBooksAPICall().Result;
             SelectedPhoneBook = AllPhoneBooks.Find(b => b.PhoneBookID == phoneBookId);
             GetAllContacts(phoneBookId);
-        }
-
-        public void GetPhoneBooks()
-        {
-            AllPhoneBooks = new List<PhoneBookObject>();
-            try
-            {
-                DataRowCollection rows = DAL.ExecuteSP("GetAllPhoneBooks").Tables[0].Rows;
-                if (rows.Count > 0)
-                {
-                    foreach (DataRow row in rows)
-                    {
-                        var phoneBook = new PhoneBookObject()
-                        {
-                            PhoneBookID = Guid.Parse(row["PhoneBookID"].ToString()),
-                            PhoneBookName = row["PhoneBookName"].ToString(),
-                        };
-                        AllPhoneBooks.Add(phoneBook);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
+        }     
 
         public void GetAllContacts( Guid phoneBookId)
         {
@@ -107,5 +84,38 @@ namespace PhoneBook.Pages
                 AllPhoneBookEntries = (List<EntryObject>)AllPhoneBookEntries.Where(s => s.EntryName.Contains(SearchString) || s.EntryNumber.Contains(SearchString));
             }
         }
+
+        /// <summary>
+        /// This methods makes and API call to fetch all the phone books
+        /// </summary>
+        /// <returns>List<PhoneBookObject></returns>
+        public async Task<List<PhoneBookObject>> GetPhoneBooksAPICall()
+        {
+            HttpMethod method = HttpMethod.Get;
+            //var URL = _configuration.GetSection("EnvironmentEndPoint").Value + "/createLead";  //ToDo - Andrea - Move this to reference a dynamic variable in AWS Parameter Store
+            var URL = "https://localhost:44338/Api/PhoneBook/GetPhoneBooks";  //TODO:: - Andrea - Move this to reference a dynamic variable in AWS Parameter Store
+            var response = string.Empty;
+
+            var httpClientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (message, cert, chain, sslPolicyErrors) =>
+                {
+                    return true;
+                }
+            };
+            using (var client = new HttpClient(httpClientHandler))
+            using (var request = new HttpRequestMessage(method, URL))
+            {
+                using (HttpResponseMessage res = client.SendAsync(request).Result)
+                {
+                    using (HttpContent content = res.Content)
+                    {
+                        response = await content.ReadAsStringAsync();
+                    }
+                }
+            }
+            return JsonConvert.DeserializeObject<List<PhoneBookObject>>(response);
+        }
+
     }
 }
